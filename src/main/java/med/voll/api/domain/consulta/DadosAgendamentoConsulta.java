b47c10996de7674e107cc2376b207c17e1c553cd
Lucas Silva <lucas.cristiano96@gmail.com>
2023-11-07 22:41:50 -0300

    Agendamento de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.consulta;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+
+        @JsonAlias("id_medico")
+        Long idMedico,
+
+        @NotNull
+        @JsonAlias("id_paciente")
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        @JsonFormat(pattern = "dd/MM/yyy HH:mm")
+        LocalDateTime data,
+
+        Especialidade especialidade) {
+}
